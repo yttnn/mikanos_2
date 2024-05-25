cc7e5fc8bc711bf5119bafb77567d621d395856c
yttnn <67893139+yttnn@users.noreply.github.com>
2024-05-25 17:45:35 +0900

    day04a + alpha

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+DEVENV_DIR=${HOME}/osbook/devenv
+EDK2_DIR=${HOME}/edk2
+WORK_DIR=${HOME}/workspace/osbook/mikanos_2
+
+cd ${EDK2_DIR}
+source edksetup.sh
+cd ${WORK_DIR}
+
+source ${DEVENV_DIR}/buildenv.sh
